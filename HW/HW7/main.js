d48f60456a5476c08707e7228641f3029e7d9817
Sanyk <145019990+OleksandrVoloshchuk@users.noreply.github.com>
2024-01-30 23:43:19 +0200

    HW7

---
+++ HW/HW7/main.js
@@ -0,0 +1,9 @@
+let num1 = prompt("Введіть перше число: ");
+let num2 = prompt("Введіть друге число: ");
+let num3 = prompt("Введіть третє число: ");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+num3 = parseFloat(num3);
+
+alert((num1 + num2 + num3) / 3);
